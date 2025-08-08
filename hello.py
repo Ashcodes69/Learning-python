44da4fda48f14fef91e09549a956d71c0488d91a
Ashish Kumar <ns730106@gmail.com>
2025-08-08 08:43:38 +0530

    nothing

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-
+num = input("enter a number: ")
+try:
+    print(int(num) + 89)
+except Exception as e:
+    print(e)
--- leetcode.py
+++ leetcode.py
@@ -1,44 +1,42 @@
-# Example 1:
-# Input: basket1 = [4,2,2,2], basket2 = [1,4,1,2]
-# Output: 1
-# Explanation: Swap index 1 of basket1 with index 0 of basket2, which has cost 1. Now basket1 = [4,1,2,2] and basket2 = [2,4,1,2]. Rearranging both the arrays makes them equal.
-# Example 2:
-# Input: basket1 = [2,3,4,1], basket2 = [3,2,5,1]
-# Output: -1
-# Explanation: It can be shown that it is impossible to make both the baskets equal.
+"""
+Example 1:
 
-basket1 = [2, 3, 4, 1]
-basket2 = [3, 2, 5, 1]
-dic = {}
-for i in basket1:
-    dic[i] = basket1.count(i)
-for i in basket2:
-    dic[i] = dic.get(i, 0) + 1
+Input: fruits = [1,2,1]
+Output: 3
+Explanation: We can pick from all 3 trees.
+Example 2:
 
-extra1 = []
-extra2 = []
-for i, count in dic.items():
-    if count % 2 != 0:
-        print(-1)
-        break
-    else:
-        count1 = basket1.count(i)
-        count2 = basket2.count(i)
-        target = (count1 + count2) // 2
-        if count1 > target:
-            for j in range(count1 - target):
-                extra1.append(i)
-        elif count2 > target:
-            for j in range(count2 - target):
-                extra2.append(i)
+Input: fruits = [0,1,2,2]
+Output: 3
+Explanation: We can pick from trees [1,2,2].
+If we had started at the first tree, we would only pick from trees [0,1].
+Example 3:
 
+Input: fruits = [1,2,3,2,2]
+Output: 4
+Explanation: We can pick from trees [2,3,2,2].
+If we had started at the first tree, we would only pick from trees [1,2].
+"""
 
-extra1.sort()
-extra2.sort(reverse=True)
+fruits = [1, 2, 3, 2, 2]
 
-minVal = min(basket1 + basket2)
-cost = 0
-for a, b in zip(extra1, extra2):
-    cost += min(2 * minVal, min(a, b))
+l = 0
+max_fruits = 0
+dictt = {}
 
-print(cost)
+# Right pointer of the sliding window
+for r in range(len(fruits)):
+    # Add the current fruit to the basket (dictionary) and update its count
+    dictt[fruits[r]] = dictt.get(fruits[r], 0) + 1
+    # If we have more than 2 types of fruits, shrink the window from the left
+    while len(dictt) >= 3:
+        # Reduce the count of the leftmost fruit
+        dictt[fruits[l]] -= 1
+        # If the count becomes 0, remove it completely from the dictionary
+        if dictt.get(fruits[l], 0) == 0:
+            del dictt[fruits[l]]
+        # Move the left pointer to the right (shrink the window)
+        l += 1
+    # Update the max fruits collected so far (valid window size)
+    max_fruits = max(max_fruits, (r - l) + 1)
+print(max_fruits)
